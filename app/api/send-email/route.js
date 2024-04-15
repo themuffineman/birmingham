88cda11ee16b317beb5060cca8945ef4cc368d81
Petrus <petrusheya@gmail.com>
2024-04-15 23:07:34 +0200

    added test

--- app/api/send-email/route.js
+++ app/api/send-email/route.js
@@ -1,5 +1,5 @@
-import nodemailer from 'nodemailer'
-import { google } from 'googleapis'
+// import nodemailer from 'nodemailer'
+// import { google } from 'googleapis'
 import sgMail from '@sendgrid/mail'
 
 // import connectDB from "@/utils/connectDB";
@@ -45,7 +45,7 @@ export async function POST(req){
 
         const mailOptions = {
             from: 'pendorastudios@gmail.com',
-            to: 'petrusheya@gmail.com',
+            to: email,
             subject: 'Would you like help with your website?',
             text: `Greetings ${name}! I hope all is well. I recently had the chance to look at your website and I believe it can be massively improved. That's why I'd like to offer you a FREE website redesign. This way, I can demonstrate its capability to convert leads and drive tangible results for your business. If you like it, we can proceed with further collaboration. If not, there's no obligation, and you won't pay a dime. Your website has the potential to be a powerful asset for your business, and I'd hate to see it go underutilized. I'm available to schedule a call to talk about your goals and how we can help you achieve them through your website. My name is Petrus by the way I help businesses turn their websites into money making assets. From your friendly neighbourhood web developer, Petrus`,
             html: `<main style='display: flex; padding: 2rem; flex-direction: column; gap: 1rem; font-family: Arial, Helvetica, sans-serif;'><header><h1 style='font-size: 1rem; color: black; font-weight: bold; margin: .5rem;'>Hello ${name}!</h1><p style='font-size: 1rem; color: black; font-weight: light; margin: .5rem;'>I hope all is well over there.</p><p style='font-size: 1rem; color: black; font-weight: light; margin: .5rem;'>I recently had the chance to look at your website and I believe it can be massively improved.</p><p style='font-size: 1rem; color: black; font-weight: light; margin: .5rem;'>That's why I'd like to offer you a <span style='font-weight: bold;'>free</span> website redesign. This way, I can demonstrate its capability to convert leads and drive tangible results for your business. If you like it, we can proceed with further collaboration. If not, there's no obligation, and you won't pay a dime.</p><p style='font-size: 1rem; color: black; font-weight: light; margin: .5rem;'>Your website has the potential to be a powerful asset for your business, and I'd hate to see it go underutilized.</p><p style='font-size: 1rem; color: black; font-weight: normal; margin: .5rem;'> I'm available to schedule a call to talk about your goals and how we can help you achieve them through your website. My name is Petrus by the way I help businesses turn their websites into money making assets.</p></header><section style="display: flex; gap: 0rem; flex-direction: column;"><p style="margin: 0; padding: 0;">From your friendly neighbourhood web developer</p><p style='font-weight: bold; margin: 0; padding: 0;'>Petrus</p></section></main>`
