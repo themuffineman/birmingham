877dbcd9084ef860f960239e9a95ef623b61ae73
Petrus <petrusheya@gmail.com>
2024-05-07 14:36:20 +0200

    Added screenshot feature

---
+++ app/api/get-template/route.js
@@ -0,0 +1,14 @@
+export async function GET(req){
+
+    try {
+        const name = req.json()
+        const result = await fetch(`http://localhost:8080/?name=${name}`)
+        const resultJSON = JSON.parse(result)
+        const src = resultJSON.src
+
+        return Response.json({src: src}, {status: 200})
+    } catch (error) {
+        console.error(error)
+        return Response.error()
+    }
+}
--- components/LeadCard.jsx
+++ components/LeadCard.jsx
@@ -2,6 +2,7 @@
 import React, { useRef, useState } from 'react'
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
 import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
+import Image from 'next/image'
   
   
 
@@ -9,8 +10,8 @@ const LeadCard = ({platform = 'google', emails, name, url, index, setLeadsData, 
 
     const [loading, setLoading] = useState(false)
     const [templateName, setTemplateName] = useState(name)
+    const [templateImage, setTemplateImage] = useState()
     const newEmail = useRef(null)
-    const templateRef = useRef(null)
 
    
     async function sendEmail(name, email){
@@ -58,6 +59,16 @@ const LeadCard = ({platform = 'google', emails, name, url, index, setLeadsData, 
             return updatedLeads;  
         });
     }
+    async function getTemplate(){
+        try {
+            const image = await fetch(`/api/get-template/?name=${templateName}`)
+            const imageJSON = await JSON.parse(image)
+            setTemplateImage(imageJSON.src)
+        } catch (error) {
+            console.error(error)
+            alert('Failed to generate image')
+        }
+    }
 
   return (
     <div className='grid grid-row-1 grid-flow-col justify-between items-center justify-items-center ring ring-slate-500 w-[70rem] rounded-md p-4'>
@@ -75,6 +86,20 @@ const LeadCard = ({platform = 'google', emails, name, url, index, setLeadsData, 
                 </div>
             </PopoverContent>
         </Popover>
+        <Dialog className="w-screen relative">
+            <DialogTrigger className="flex gap-1 bg-neutral-300 rounded-md p-2 text-base font-semibold">
+                Preview Template
+                <svg className='fill-black' xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-480H200v480Zm280-80q-82 0-146.5-44.5T240-440q29-71 93.5-115.5T480-600q82 0 146.5 44.5T720-440q-29 71-93.5 115.5T480-280Zm0-60q56 0 102-26.5t72-73.5q-26-47-72-73.5T480-540q-56 0-102 26.5T306-440q26 47 72 73.5T480-340Zm0-100Zm0 60q25 0 42.5-17.5T540-440q0-25-17.5-42.5T480-500q-25 0-42.5 17.5T420-440q0 25 17.5 42.5T480-380Z"/></svg>
+            </DialogTrigger>
+            <DialogContent className="w-[90vw]">
+                <Image
+                alt='templateImage'
+                layout='fill'
+                src={templateImage}
+                className='w-100% h-auto object-cover'
+                />
+            </DialogContent>
+        </Dialog>
         <Popover>
             <PopoverTrigger>
                 <div className="flex gap-1 bg-neutral-300 rounded-md p-2 text-base font-semibold">
@@ -136,6 +161,10 @@ const LeadCard = ({platform = 'google', emails, name, url, index, setLeadsData, 
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 48 48"><path fill="#DD2C00" d="M10.7,32.7c-0.5,0-0.9-0.3-1.2-0.8c-0.2-0.4-0.3-1-0.4-1.7c-0.2-2.2,0-5.5,0.7-6.5c0.3-0.5,0.8-0.7,1.2-0.7c0.3,0,0.6,0.1,7.1,2.8c0,0,1.9,0.8,1.9,0.8c0.7,0.3,1.1,1,1.1,1.8c0,0.8-0.5,1.4-1.2,1.6c0,0-2.7,0.9-2.7,0.9C11.2,32.7,11,32.7,10.7,32.7z M24,36.3c0,6.3,0,6.5-0.1,6.8c-0.2,0.5-0.6,0.8-1.1,0.9c-1.6,0.3-6.6-1.6-7.7-2.8c-0.2-0.3-0.3-0.5-0.4-0.8c0-0.2,0-0.4,0.1-0.6c0.1-0.3,0.3-0.6,4.8-5.9c0,0,1.3-1.6,1.3-1.6c0.4-0.6,1.3-0.7,2-0.5c0.7,0.3,1.2,0.9,1.1,1.6C24,33.5,24,36.3,24,36.3z M22.8,22.9c-0.3,0.1-1.3,0.4-2.5-1.6c0,0-8.1-12.9-8.3-13.3c-0.1-0.4,0-1,0.4-1.4c1.2-1.3,7.7-3.1,9.4-2.7c0.6,0.1,0.9,0.5,1.1,1c0.1,0.6,0.9,12.5,1,15.2C24.1,22.5,23.1,22.8,22.8,22.9z M27.2,25.9c-0.4-0.6-0.4-1.4,0-1.9c0,0,1.7-2.3,1.7-2.3c3.6-5,3.8-5.3,4.1-5.4c0.4-0.3,0.9-0.3,1.4-0.1c1.4,0.7,4.4,5.1,4.6,6.7c0,0,0,0,0,0.1c0,0.6-0.2,1-0.6,1.3c-0.3,0.2-0.5,0.3-7.4,1.9c-1.1,0.3-1.7,0.4-2,0.5c0,0,0-0.1,0-0.1C28.4,26.9,27.6,26.5,27.2,25.9z M38.9,34.4c-0.2,1.6-3.5,5.8-5.1,6.4c-0.5,0.2-1,0.2-1.4-0.2c-0.3-0.2-0.5-0.6-4.1-6.4l-1.1-1.7c-0.4-0.6-0.3-1.4,0.2-2.1c0.5-0.6,1.2-0.8,1.9-0.6c0,0,2.7,0.9,2.7,0.9c6,2,6.2,2,6.4,2.2C38.8,33.4,39,33.9,38.9,34.4z"></path></svg>}
         </div>
         <div className='flex gap-3 items-center '>
+            <button onClick={()=> getTemplate()} className="p-2 w-max flex gap-3 items-center rounded-md hover:ring active:translate-y-1 transition-transform hover:ring-black text-white bg-yellow-500 hover:text-black hover:bg-yellow-300 text-base font-semibold">
+                <span>Generate Template </span>
+                {imageLoading && <span className='size-4 rounded-full border-2 border-t-neutral-400 animate-spin'/>}
+            </button>
             <button onClick={()=> sendEmail(name, emails[0])} className="p-2 w-max flex gap-3 items-center rounded-md hover:ring active:translate-y-1 transition-transform hover:ring-black text-white bg-yellow-500 hover:text-black hover:bg-yellow-300 text-base font-semibold">
                 <span>Send Email</span>
                 {loading && <span className='size-4 rounded-full border-2 border-t-neutral-400 animate-spin'/>}
